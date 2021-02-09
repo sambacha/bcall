4b761299079b206d68e5b2139582a79e1ade46cb
sam <sam@freighttrust.com>
2021-02-08 20:57:40 -0800

    init

---
+++ .prettierrc.js
@@ -0,0 +1,27 @@
+/**
+* @file Prettier configuration for Solidity
+* @version 1.0.2 
+* @license CC0-1.0
+*/
+module.exports = {
+  arrowParens: "avoid",
+  bracketSpacing: true,
+  endOfLine: "auto",
+  printWidth: 100,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "all",
+  overrides: [
+    {
+      files: "*.sol",
+      options: {
+        printWidth: 120,
+        tabWidth: 4,
+        useTabs: false,
+        singleQuote: false,
+        bracketSpacing: false,
+        explicitTypes: "always",
+      }
+    }
+  ]
+};
